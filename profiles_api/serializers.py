f27500fe115e8acdb1288e8b544108d9d64ab7a8
Mjd Akel <146154283+MjdAkel@users.noreply.github.com>
2025-12-30 15:20:07 +0200

    Add files via upload

---
+++ profiles_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+from rest_framework import serializers
+
+class HelloSerializer(serializers.Serializer):
+    """Serializer for testing"""
+    name = serializers.CharField(max_length=10)
